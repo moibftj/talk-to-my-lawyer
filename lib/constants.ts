45820b82df4474048eb0986860b6b0efc8b0dd3c
moizjamil <199167745+moibftj@users.noreply.github.com>
2026-01-18 14:33:28 +0000

    fix: update logo source file extension from .jpg to .svg for consistency

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
-export const DEFAULT_LOGO_SRC = '/talk-to-my-lawyer-logo.jpg'
+export const DEFAULT_LOGO_SRC = '/talk-to-my-lawyer-logo.svg'
 export const DEFAULT_LOGO_ALT = 'Talk-To-My-Lawyer logo'
 
 /**
